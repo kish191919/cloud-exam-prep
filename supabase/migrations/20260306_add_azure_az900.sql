c661e0839c6b9ad8ffa2b3d33774b7f97fe6105f
Danny <kish1919@gmail.com>
2026-03-06 13:10:42 -0500

    updates

---
+++ supabase/migrations/20260306_add_azure_az900.sql
@@ -0,0 +1,11 @@
+INSERT INTO exams (id, title, code, certification, description, time_limit_minutes, passing_score, version)
+VALUES (
+  'azure-az-900',
+  'Microsoft Azure Fundamentals',
+  'AZ-900',
+  'Azure',
+  'Azure 클라우드 기초 개념, 핵심 서비스, 보안, 개인 정보 보호, 규정 준수 및 가격 책정을 다루는 입문 자격증.',
+  45,
+  70,
+  '2026.03'
+);
